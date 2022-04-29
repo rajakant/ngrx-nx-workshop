90747585a47b2ade89169fab4ea7ad49561a733c
Rajat Kanthaliya <rajakant@cisco.com>
2022-04-29 11:37:58 +0530

    remove unnecessary  import

--- apps/ngrx-workshop/src/app/product/effects.ts
+++ apps/ngrx-workshop/src/app/product/effects.ts
@@ -1,19 +1,16 @@
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { catchError } from 'rxjs/operators';
 import { ProductService } from './product.service';
-import { ApplicationRef, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import * as productLisrActions from './product-list/actions';
 import * as apiActions from './action';
-import { exhaustMap, map, of, tap } from 'rxjs';
+import { exhaustMap, map, of } from 'rxjs';
 
 @Injectable()
 export class ProductsEffects {
   constructor(
     private readonly actions$: Actions,
-    private readonly ProductService: ProductService,
-    private readonly snackBar: MatSnackBar,
-    private readonly appRef: ApplicationRef
+    private readonly ProductService: ProductService
   ) {}
 
   fetchProducts$ = createEffect(() => {
