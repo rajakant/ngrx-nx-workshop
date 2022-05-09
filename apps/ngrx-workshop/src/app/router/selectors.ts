e22db4bd7083b917cdc7ba6384101fe3b6f74353
Rajat Kanthaliya <rajakant@cisco.com>
2022-05-09 14:07:22 +0530

    added router store

---
+++ apps/ngrx-workshop/src/app/router/selectors.ts
@@ -0,0 +1,16 @@
+import {
+  getSelectors,
+  MinimalRouterStateSnapshot,
+  RouterReducerState,
+} from '@ngrx/router-store';
+import { createFeatureSelector } from '@ngrx/store';
+
+export const ROUTER_FEATURE_KEY = 'router';
+
+export const routerFeatureState =
+  createFeatureSelector<RouterReducerState<MinimalRouterStateSnapshot>>(
+    ROUTER_FEATURE_KEY
+  );
+
+export const getRouterParam =
+  getSelectors(routerFeatureState).selectCurrentRoute;
