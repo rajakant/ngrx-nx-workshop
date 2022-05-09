228df12c35487afd274979b7f59dc3296895b4d1
Rajat Kanthaliya <rajakant@cisco.com>
2022-05-09 13:36:07 +0530

    cache product data

--- apps/ngrx-workshop/src/app/product/effects.ts
+++ apps/ngrx-workshop/src/app/product/effects.ts
@@ -1,4 +1,4 @@
-import { catchError } from 'rxjs/operators';
+import { catchError, withLatestFrom, filter } from 'rxjs/operators';
 import { ProductService } from './product.service';
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
@@ -6,12 +6,16 @@ import * as productLisrActions from './product-list/actions';
 import * as apiActions from './action';
 import { exhaustMap, map, of } from 'rxjs';
 import * as cartDetailsAction from '../cart/cart-details/actions';
+import { Store } from '@ngrx/store';
+import { GlobalState } from './reducer';
+import * as selectors from './selectors';
 
 @Injectable()
 export class ProductsEffects {
   constructor(
     private readonly actions$: Actions,
-    private readonly ProductService: ProductService
+    private readonly productService: ProductService,
+    private readonly store: Store<GlobalState>
   ) {}
 
   fetchProducts$ = createEffect(() => {
@@ -20,8 +24,12 @@ export class ProductsEffects {
         productLisrActions.productsOpened,
         cartDetailsAction.cartDetailsOpened
       ),
+      withLatestFrom(this.store.select(selectors.getProducts)),
+      filter(([, products]) => {
+        return !(products && products.length);
+      }),
       exhaustMap(() =>
-        this.ProductService.getProducts().pipe(
+        this.productService.getProducts().pipe(
           map((products) => apiActions.productsFetchedSuccess({ products })),
           catchError(() =>
             of(
